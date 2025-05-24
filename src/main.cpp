07dabf0a8393d7d609b8023808bbce1e0552944c
myrzaarslan <arsssslan@mail.ru>
2025-05-25 02:09:07 +0500

    echo attempt 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include <string>
-#include <string_view>
+#include <sstream>
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-
+  std::string input;
   while (true) {
     std::cout << "$ ";
-    std::string input;
-    std::getline(std::cin, input);
-    if (input == "exit 0") {
-      break;
-    }
-    if (input.contains("echo"))
-    else {
+
+    std::istringstream iss(input);
+    std::string command;
+    iss >> command;
+
+    if (command == "exit") {
+      std::string code;
+      iss >> code;
+      if (code == "0") break;
+    } else if (command == "echo") {
+      std::string word;
+      bool first = true;
+      while (iss >> word) {
+        if (!first) std::cout << " ";
+        std::cout << word;
+        first = false;
+      }
+      std::cout << std::endl;
+    } else {
       std::cout << input << ": command not found" << std::endl;
     }
   }
+
+  return 0;
 }
