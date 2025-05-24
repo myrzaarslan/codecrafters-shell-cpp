c1abc28c8032e77c0b6f70782226b1126431ff2a
myrzaarslan <arsssslan@mail.ru>
2025-05-25 01:26:48 +0500

    repl attempt 2

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ int main() {
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
 
   while (true) {
+    std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
     std::cout << input << ": command not found" << std::endl;
