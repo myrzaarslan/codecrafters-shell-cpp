38e9a53ba3032915a1ecdba6bfc73ae1534d54a4
myrzaarslan <arsssslan@mail.ru>
2025-05-25 12:43:39 +0500

    type command implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
+#include <algorithm>
 
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  std::vector<std::string> commands = {"exit", "echo", "type"};
 
   while (true) {
     std::string input;
@@ -28,6 +32,15 @@ int main() {
         first = false;
       }
       std::cout << std::endl;
+    } else if (command == "type") {
+      std::string word;
+      iss >> word;
+      if (std::find(commands.begin(), commands.end(), word) != commands.end()) {
+        std::cout << input << " is a shell builtin" << std::endl;
+      }
+      else {
+        std::cout << word << ": not found" << std::endl;
+      }
     } else {
       std::cout << input << ": command not found" << std::endl;
     }
