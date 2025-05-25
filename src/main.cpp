0e38c270d6a89f543f4b210865cfe48099b514c5
myrzaarslan <arsssslan@mail.ru>
2025-05-25 13:37:21 +0500

    PATH implemented

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 int main() {
   std::cout << std::unitbuf;
@@ -35,11 +39,33 @@ int main() {
     } else if (command == "type") {
       std::string word;
       iss >> word;
+
       if (std::find(commands.begin(), commands.end(), word) != commands.end()) {
-        std::cout << word << " is a shell builtin" << std::endl;
-      }
-      else {
-        std::cout << word << ": not found" << std::endl;
+          std::cout << word << " is a shell builtin" << std::endl;
+      } else {
+          const char* path_env = std::getenv("PATH");
+          if (path_env) {
+              std::string path_str = path_env;
+              std::istringstream path_stream(path_str);
+              std::string dir;
+              bool found = false;
+
+              while (std::getline(path_stream, dir, ':')) {
+                  fs::path full_path = fs::path(dir) / word;
+                  if (fs::exists(full_path) && fs::is_regular_file(full_path) &&
+                      (fs::status(full_path).permissions() & fs::perms::owner_exec) != fs::perms::none) {
+                      std::cout << word << " is " << full_path << std::endl;
+                      found = true;
+                      break;
+                  }
+              }
+
+              if (!found) {
+                  std::cout << word << ": not found" << std::endl;
+              }
+          } else {
+              std::cout << word << ": not found" << std::endl;
+          }
       }
     } else {
       std::cout << input << ": command not found" << std::endl;
